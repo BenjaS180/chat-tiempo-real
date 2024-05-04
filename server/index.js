8d6e3d24023d844ee8fce3dc5d610a5e6cffd5cf
unknown <soporte@CLFDSK_Bsoto.GItahue.local>
2024-05-04 17:50:46 -0400

    Web sockets comunicandose con servidor

--- server/index.js
+++ server/index.js
@@ -1,30 +1,64 @@
-import express from 'express'
-import logger from 'morgan'
-
+// Servidor Intermediario
+import express from 'express';
+import logger from 'morgan';
 import { Server } from 'socket.io';
-import { createServer } from 'node:http';
+import { createServer } from 'http';
+import net from 'net';
 
-const port = process.env.PORT ?? 3000;
-
+const port = process.env.PORT ?? 8000;
 
 const app = express();
-const server = createServer(app)
-const io = new Server(server)
-
-io.on('connection',(socket) => {
-    console.log('a user has connected!')
-
-    socket.on('disconnect',() =>{
-        console.log('an user has disconnected')
-    })
-})
-
-app.use(logger('dev'))
-
-app.get('/',(req,res) =>{
-    res.sendFile(process.cwd() + '/client/index.html')
+const server = createServer(app);
+const io = new Server(server,{
+    connectionStateRecovery
 });
 
-server.listen(port,()=>{
-    console.log(`Server running on port ${port}`)
+// Configurar cliente TCP para comunicarse con el servidor remoto
+const serverAddress = '172.20.8.6';
+const serverPort = 1234;
+const nickName = 'Tesoreria(Prueba)';
+const clientSocket = net.createConnection({ host: serverAddress, port: serverPort });
+
+// Manejar eventos de conexión y desconexión del cliente TCP
+clientSocket.on('connect', () => {
+    console.log('Connected to remote server');
+});
+
+clientSocket.on('error', (error) => {
+    console.error('Error connecting to remote server:', error);
+});
+
+clientSocket.on('close', () => {
+    console.log('Disconnected from remote server');
+});
+
+
+clientSocket.on('data', (data) => {
+    const mensaje = data.toString();
+    if (!mensaje.includes(nickName)) {
+        io.emit('mensajeDesdeServidor', mensaje);
+    }
+});
+
+// Escuchar mensajes de los clientes y enviarlos al servidor remoto a través del cliente TCP
+io.on('connection', (socket) => {
+    console.log('A user has connected!');
+
+    socket.on('sendMessageToServer', (message) => {
+        clientSocket.write(`MSG\x02${nickName}\x02${message}\x04`);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user has disconnected');
+    });
+});
+
+app.use(logger('dev'));
+
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + '/client/index.html');
+});
+
+server.listen(port, '172.20.10.4', () => {
+    console.log(`Server running on http://172.20.10.4:${port}`);
 });
